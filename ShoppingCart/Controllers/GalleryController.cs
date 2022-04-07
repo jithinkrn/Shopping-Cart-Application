01a49c2f8bec9f25eaa58c2c9e6ec6b60c7c5973
JITHIN <JITHIN@LAPTOP-JJVRNRQR>
2022-04-07 18:35:22 +0800

    ok

--- ShoppingCart/Controllers/GalleryController.cs
+++ ShoppingCart/Controllers/GalleryController.cs
@@ -15,7 +15,7 @@ namespace ShoppingCart.Controllers
         public GalleryController(DBContext dbContext)
         {
 
-            //this comment is from gab reyes + jithin's comment ascadsfbva ..... 
+            //this comment is from gab reyes + jithin's comment ascadsfbva ..... sss
 
             this.dbContext = dbContext;
             //TEst
