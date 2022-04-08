af488e81ebf3e6cda88868687e6f34c7ac69aac1
JITHIN <JITHIN@LAPTOP-JJVRNRQR>
2022-04-08 14:42:30 +0800

    ok

--- ShoppingCart/DB.cs
+++ ShoppingCart/DB.cs
@@ -11,7 +11,7 @@ namespace ShoppingCart
 {
     public class DB
     {
-       //test1
+       //Change from jithin branch
         private DBContext dbContext;
 
         public DB(DBContext dbContext)
